cad13f90b60f46024a10d3ac66bd59b7fa70e410
shubhamm1525 <code4shubham@gmail.com>
2023-03-08 23:37:47 +0530

    Added javascript method to delete the elements in the taskLists Array

--- force-app/main/default/lwc/todo/todo.js
+++ force-app/main/default/lwc/todo/todo.js
@@ -1,3 +1,40 @@
-import { LightningElement } from 'lwc';
+import { LightningElement,track } from 'lwc';
 
-export default class Todo extends LightningElement {}
+export default class Todo extends LightningElement {
+    @track newTask;
+    
+    @track
+    todoTasks = [
+    ];
+
+    updateNewTask(event){
+        this.newTask = event.target.value;
+    }
+
+    addTaskToList(event) {
+        console.log(this.newTask);
+        if(this.newTask){        
+            this.todoTasks.push({
+                id: this.todoTasks.length + 1,
+                name : this.newTask
+            });
+            this.newTask = '';
+        }
+    }
+
+    deleteTaskFromList(event){
+
+        console.log('### DELETE Task for ID :::' + event.target.name);
+
+        let idToDelete =  event.target.name;
+        let todoTasks =  this.todoTasks;
+        let indexToDelete;
+
+        for(var i = 0; i < todoTasks.length; i++ ){
+            if(idToDelete == todoTasks[i].id){
+                indexToDelete = i;
+            }
+        }
+        this.todoTasks.splice(indexToDelete,1);        
+    }
+}
