de3bfc1c339e398ba28646515879818d2a786b14
shubhamm1525 <code4shubham@gmail.com>
2023-03-15 22:08:25 +0530

    Optimized Code, Added spinner on add, delete, refresh

--- force-app/main/default/lwc/todo/todo.js
+++ force-app/main/default/lwc/todo/todo.js
@@ -1,11 +1,15 @@
 import { LightningElement,track,wire } from 'lwc';
 import { refreshApex } from '@salesforce/apex';
 import getTasks from '@salesforce/apex/ToDoListController.getTasks';
+import insertTask from '@salesforce/apex/ToDoListController.insertTask';
+import deleteTask from '@salesforce/apex/ToDoListController.deleteTask';
 
 export default class Todo extends LightningElement {
-    @track newTask;
+    @track newTask = '';
     error;
     toDoTasksResponse;
+
+    processing = true;
     
     @track
     todoTasks = [];
@@ -16,14 +20,28 @@ export default class Todo extends LightningElement {
 
     /* Method to add task to List */
     addTaskToList(event) {
-        console.log(this.newTask);
-        if(this.newTask){        
-            this.todoTasks.push({
-                Id: this.todoTasks.length + 1,
-                Subject : this.newTask,
-            });
-            this.newTask = '';
+        
+        if(this.newTask == ''){
+            return;
         }
+
+        this.processing = true;
+        insertTask({subject : this.newTask})
+        .then(result => {
+            this.processing = false;
+                if(this.newTask){        
+                    this.todoTasks.push({
+                        Id: this.todoTasks.length + 1,
+                        Subject : this.newTask,
+                        recordId : result.Id
+                    });
+                    this.newTask = '';
+                }
+            }
+        )
+        .error(error => { 
+            console.log(error);
+        });
     }
 
     deleteTaskFromList(event){
@@ -33,25 +51,35 @@ export default class Todo extends LightningElement {
         let idToDelete =  event.target.name;
         let todoTasks =  this.todoTasks;
         let indexToDelete;
+        let recordIdToDelete;
 
         /* Basic looping method to loop through todotasks items and 
         comparing the Id of todoTask iteam with selected Task from UI */
-        // for(var i = 0; i < todoTasks.length; i++ ){
-        //     if(idToDelete == todoTasks[i].id){
-        //         indexToDelete = i;
-        //     }
-        // }
-        //this.todoTasks.splice(indexToDelete,1);  
+        for(var i = 0; i < todoTasks.length; i++ ){
+            if(idToDelete == todoTasks[i].Id){
+                indexToDelete = i;
+            }
+        }
 
+        recordIdToDelete  = todoTasks[indexToDelete].recordId;
+
+        this.processing = true;
+
+        deleteTask({recordId:recordIdToDelete})
+        .then(result =>{
+            this.processing = false;
         // splice method with findIndex method to find the index 
         // remove single element from list
-        // todoTasks.splice(todoTasks.findIndex((todoTask)=> {
-        //     return todoTask.id === indexToDelete;
-        // }),1);
-
-        todoTasks.splice(todoTasks.findIndex(item => idToDelete === item.id),1);
-
-              
+            if(result){
+                todoTasks.splice(indexToDelete,1);    
+                console.log(result);
+            }else{
+                console.log("Unable to delete task");
+            }
+        }) 
+        .error(error => {
+            console.log(error);
+        });           
     }
 
     @wire(getTasks)
@@ -60,6 +88,10 @@ export default class Todo extends LightningElement {
         this.toDoTasksResponse = response;
         let data  = response.data;
         let error = response.error;
+
+        if(data || error){
+            this.processing = false;
+        }
 
         if(data){
             this.todoTasks = [];
@@ -77,6 +109,8 @@ export default class Todo extends LightningElement {
     }
 
     refreshTodoList(){
-        refreshApex(this.toDoTasksResponse);
+        this.processing = true;
+        refreshApex(this.toDoTasksResponse)
+        .finally(() => this.processing = false);
     }
-}
+}   
